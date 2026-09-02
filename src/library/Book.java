ed5ee54f90e73962d46987d141fc0d3f2c279921
tvnvk222 <tvnvk122@cole.tvnk>
2026-09-02 13:08:35 +0200

    feat: add parameterized and default constructors to Book

--- src/library/Book.java
+++ src/library/Book.java
@@ -4,4 +4,19 @@ public class Book extends Item{
     private String author;
     private int pages;
     private String genre;
+
+    //default constructor
+    Book(){
+        super();
+        this.author="Unknown";
+        this.pages=0;
+        this.genre="Unknown";
+    }
+    //parametwrized constructor
+    Book(int ID, String title, int year, boolean Borrowed, int duration, String author, int pages, String genre){
+        super(int ID, String title, int year, boolean Borrowed, int duration);
+        this.author=author;
+        this.pages=pages;
+        this.genre=genre;
+    }
 }
