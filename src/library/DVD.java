2eebcd283407780bd6d72bd36712e4e5edabb53b
tvnvk222 <tvnvk122@cole.tvnk>
2026-09-02 13:09:24 +0200

    feat: add parameterized and default constructors to DVD

--- src/library/DVD.java
+++ src/library/DVD.java
@@ -4,4 +4,18 @@ public class DVD extends Item {
     private String director;
     private String durationInMinutes;
     private int rating;
+    //default constructor
+    DVD(){
+        super();
+        this.director="Unknown";
+        this.durationInMinutes="Unknown";
+        this.rating=0;
+    }
+    //parametwrized constructor
+    DVD(int ID, String title, int year, boolean Borrowed, int duration, String director, String durationInMinutes, int rating){
+        super(int ID, String title, int year, boolean Borrowed, int duration);
+        this.director=director;
+        this.durationInMinutes=durationInMinutes;
+        this.rating=rating;
+    }
 }
