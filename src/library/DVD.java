6945e945ff6248f51ecad11758e9773d5514484c
tvnvk222 <tvnvk122@cole.tvnk>
2026-09-02 15:37:49 +0200

    refactor: in DVD getters set this.rating instead of this.ratingrating

--- src/library/DVD.java
+++ src/library/DVD.java
@@ -11,7 +11,7 @@ public class DVD extends Item {
         this.durationInMinutes="Unknown";
         this.rating=0;
     }
-    //parametwrized constructor
+    //parameterized constructor
     public DVD(int ID, String title, int year, boolean Borrowed, int duration, String director, String durationInMinutes, int rating){
         super(ID, title,year, Borrowed, duration);
         this.director=director;
@@ -21,7 +21,7 @@ public class DVD extends Item {
     //getters
     public String getDirector() {return this.director;}
     public String getDurationInMinutes() {return this.durationInMinutes;}
-    public int getRating() {return this.ratingating;}
+    public int getRating() {return this.rating;}
     //setters
     public void setDirector(String director) {this.director = director;}
     public void setDurationInMinutes(String durationInMinutes) {this.durationInMinutes = durationInMinutes;}
