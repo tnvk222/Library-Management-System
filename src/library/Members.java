7556081213df7d6971ccd8058029c3bf189491b0
tvnvk222 <tvnvk122@cole.tvnk>
2026-09-02 13:14:40 +0200

    feat: add parameterized and default constructor to Members

--- src/library/Members.java
+++ src/library/Members.java
@@ -3,4 +3,15 @@ package library;
 public class Members {
  int ID;
  String name;
+
+ //default constructor
+ Members(){
+  this.ID=0;
+  this.name="Unknown";
+ }
+ //parameterized constructor
+ Members(int ID, String name){
+  this.ID=ID;
+  this.name=name;
+ }
 }
