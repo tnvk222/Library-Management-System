323ac67e3e825abeb0702e2f5c36f5e326ac7e18
tvnvk222 <tvnvk122@cole.tvnk>
2026-09-02 12:40:58 +0200

    feat: add parameterized and default constructors to Item class

--- src/library/Item.java
+++ src/library/Item.java
@@ -6,4 +6,20 @@ public class Item {
     private int yearPublished;
     boolean isBorrowed;
     int borrowingDuration;
+
+    //default constructor
+    Item(){
+        this.ID=0;
+        this.title="Unknown";
+        this.yearPublished=0000;
+        this.isBorrowed=False;
+        this.borrowingDuration=0
+    }
+    Item(int ID, String title, int year, boolean Borrowed, int duration){
+        this.ID=ID;
+        this.title=title;
+        this.yearPublished=year;
+        this.isBorrowed=Borrowed;
+        this.borrowingDuration=duration;
+    }
 }
