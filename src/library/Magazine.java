b1d79532184c4979b33818db333e1e67eb25a5af
tvnvk222 <tvnvk122@cole.tvnk>
2026-09-02 13:09:58 +0200

    feat: add parameterized and default constructors to Magazine

--- src/library/Magazine.java
+++ src/library/Magazine.java
@@ -3,4 +3,17 @@ package library;
 public class Magazine extends Item {
     private String publisher;
     private int releasedNumber;
+
+    //default constructor
+    Magazine(){
+        super();
+        this.publisher="Unknown";
+        this.releasedNumber=0
+    }
+    //parameterized constructor
+    Magazine(int ID, String title, int year, boolean Borrowed, int duration, String publisher, int numberReleased){
+        super(int ID, String title, int year, boolean Borrowed, int duration);
+        this.publisher=publisher;
+        this.releasedNumber=numberReleased;
+    }
 }
