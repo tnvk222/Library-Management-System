76f297fd576d6e516b94a89cb6858a0cd4c59109
tvnvk222 <tvnvk122@cole.tvnk>
2026-09-03 10:56:43 +0200

    refactor: in DVD member fields, getters and setters set durationInMinutes to int instead of String

--- src/library/DVD.java
+++ src/library/DVD.java
@@ -1,18 +1,19 @@
 package library;
+import java.util.Scanner;
 
 public class DVD extends Item {
     private String director;
-    private String durationInMinutes;
+    private int durationInMinutes;
     private int rating;
     //default constructor
     public DVD(){
         super();
         this.director="Unknown";
-        this.durationInMinutes="Unknown";
+        this.durationInMinutes=0;
         this.rating=0;
     }
     //parameterized constructor
-    public DVD(int ID, String title, int year, boolean Borrowed, int duration, String director, String durationInMinutes, int rating){
+    public DVD(int ID, String title, int year, boolean Borrowed, int duration, String director, int durationInMinutes, int rating){
         super(ID, title,year, Borrowed, duration);
         this.director=director;
         this.durationInMinutes=durationInMinutes;
@@ -20,10 +21,10 @@ public class DVD extends Item {
     }
     //getters
     public String getDirector() {return this.director;}
-    public String getDurationInMinutes() {return this.durationInMinutes;}
+    public int getDurationInMinutes() {return this.durationInMinutes;}
     public int getRating() {return this.rating;}
     //setters
     public void setDirector(String director) {this.director = director;}
-    public void setDurationInMinutes(String durationInMinutes) {this.durationInMinutes = durationInMinutes;}
+    public void setDurationInMinutes(int durationInMinutes) {this.durationInMinutes = durationInMinutes;}
     public void setRating(int rating) {this.rating = rating;}
 }
