3884edf61344354f58f5682ddf16fb2caffc7c88
tvnvk222 <tvnvk122@cole.tvnk>
2026-09-03 11:36:31 +0200

    feat: add a user override input method in Magazine

--- src/library/Magazine.java
+++ src/library/Magazine.java
@@ -1,5 +1,5 @@
 package library;
-
+import  java.util.Scanner;
 public class Magazine extends Item {
     private String publisher;
     private int releasedNumber;
@@ -23,4 +23,25 @@ public class Magazine extends Item {
     //setters
     public void setPublisher(String publisher) {this.publisher = publisher;}
     public void setReleasedNumber(int releasedNumber) {this.releasedNumber = releasedNumber;}
+    @Override
+    public void input() {
+        Scanner input = new Scanner(System.in);
+        String publisher; int released;
+        System.out.print("Enter publisher: ");
+        setPublisher(publisher=input.next());
+        System.out.print("Enter release number: ");
+        setReleasedNumber(released= input.nextInt());
+    }
+    @Override
+    public void display() {
+        System.out.println("📰 MAGAZINE");
+        System.out.println("   ID: " + getID());
+        System.out.println("   Title: " + getTitle());
+        System.out.println("   Year: " + getYearPublished());
+        System.out.println("   Borrowed: " + (getIsBorrowed() ? "Yes" : "No"));
+        System.out.println("   Duration: " + getBorrowingDuration() + " days");
+        System.out.println("   Publisher: " + getPublisher());
+        System.out.println("   Release Number: " + getReleasedNumber());
+        System.out.println("   --------------------");
+    }
 }
